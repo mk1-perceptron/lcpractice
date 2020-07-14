2e132cb65de807b79189b4bb771683c2974f4c38
Michael Kennedy <michaeljkennedy@protonmail.com>
2020-07-14 12:12:16 -0400

    Added rust two-sum

---
+++ rust/two-sum/src/main.rs
@@ -0,0 +1,47 @@
+use std::collections::HashMap;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut remainders = HashMap::with_capacity(nums.len());
+
+    for (index, val) in nums.iter().enumerate() {
+        match remainders.get(val) {
+            Some(&i) => return vec![i as i32, index as i32],
+            None => {
+                remainders.insert(target - val, index);
+            }
+        }
+    }
+
+    vec![]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_one() {
+        let input = vec![2, 7, 11, 15];
+        let target = 9;
+        let result = two_sum(input, target);
+
+        assert!(result.len() == 2);
+        assert!(result[0] == 0);
+        assert!(result[1] == 1);
+    }
+
+    #[test]
+    fn test_two() {
+        let input = vec![2, 1, 0, -1, -2];
+        let target = 0;
+        let result = two_sum(input, target);
+
+        assert!(result.len() == 2);
+        assert!(result[0] == 1);
+        assert!(result[1] == 3);
+    }
+}
