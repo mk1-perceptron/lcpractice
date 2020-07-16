6452712ab29d96659826aafaff74fb4899e931c1
Michael Kennedy <michaeljkennedy@protonmail.com>
2020-07-16 11:33:54 -0400

    Added rust longest common prefix

---
+++ rust/longest-common-prefix/src/lib.rs
@@ -0,0 +1,53 @@
+fn longest_common_prefix(strs: Vec<String>) -> String {
+    if strs.is_empty() {
+        return "".to_owned();
+    }
+
+    let mut common = String::new();
+    let mut iters = strs.iter().map(|s| s.chars()).collect::<Vec<_>>();
+
+    for _ in 0..strs[0].len() {
+        let chars = iters.iter_mut().map(|i| i.next()).collect::<Vec<_>>();
+        let c1 = chars[0].unwrap();
+
+        if chars.iter().all(|o| match o {
+            Some(c) => *c == c1,
+            None => false,
+        }) {
+            common.push(c1);
+        } else {
+            break;
+        }
+    }
+
+    common
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_one() {
+        let input = vec!["flower".to_owned(), "flow".to_owned(), "flight".to_owned()];
+        let result = longest_common_prefix(input);
+
+        assert_eq!(result, "fl");
+    }
+
+    #[test]
+    fn example_two() {
+        let input = vec!["dog".to_owned(), "racecar".to_owned(), "car".to_owned()];
+        let result = longest_common_prefix(input);
+
+        assert_eq!(result, "");
+    }
+
+    #[test]
+    fn single_string() {
+        let input = vec!["a".to_owned()];
+        let result = longest_common_prefix(input);
+
+        assert_eq!(result, "a");
+    }
+}
